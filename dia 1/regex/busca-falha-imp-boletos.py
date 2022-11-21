1e7e0901c7905f19936465bc6f3a1a493008d18a
jomrs <Jonathanmotareis@gmail.com>
2022-11-21 14:33:32 -0300

    chore: removido close desncessario no metodo.

--- dia 1/regex/busca-falha-imp-boletos.py
+++ dia 1/regex/busca-falha-imp-boletos.py
@@ -7,6 +7,5 @@ regex = "[a-zA-z]{3}[0-9]{6}\s{2}[a-zA-z][0-9]{8}\s{4}[a-zA-z]{2}[0-9]{32}"
 with open('boletos-importacao.txt', 'r') as importacao:
     linhas = importacao.readlines()
     [linhas_com_erro.append(linhas.index(x)+1) for x in linhas if not re.findall(regex, x)]
-    importacao.close()
 
 print("Posições com erro: {}".format(linhas_com_erro))
