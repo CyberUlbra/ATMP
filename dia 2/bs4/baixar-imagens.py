69c3c72ba42784848f98e2bc1a804325db77b1d2
jomrs <Jonathanmotareis@gmail.com>
2022-11-21 22:05:35 -0300

    feat: adicionado metodo para baixar imagem aleatoria do xkcd.

---
+++ dia 2/bs4/baixar-imagens.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+baixa = requests.get("https://c.xkcd.com/random/comic/")
+soup = BeautifulSoup(baixa.text, 'html.parser')
+url_img = soup.select('div#comic img')[0]['src']
+download_url = "https:{}".format(url_img)
+
+print("bainxando: {} ...".format(download_url))
+
+with open('xkcd-scrapped.png', 'wb') as image: 
+    image.write(requests.get(download_url).content)
